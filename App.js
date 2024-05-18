88789f7b356f04954725b2969a29f6ae6100b2ae
Alon <alon@nagish.com>
2024-05-18 16:28:38 -0400

    reproducible expo-contacts serialization bug

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import * as Contacts from 'expo-contacts';
 
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+     
+      if (status !== 'granted') {
+        return;
+      }
+
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.Emails],
+      });
+
+      if (data.length > 0) {
+        const contact = data[0];
+        console.log(contact);
+      }
+
+      const contact = {
+        [Contacts.Fields.FirstName]: 'Problematic',
+        [Contacts.Fields.LastName]: 'Contact',
+        [Contacts.Fields.Company]: 'Young Money',
+        [Contacts.Fields.PhoneNumbers]: [{
+          label: Contacts.Fields.PhoneNumbers,
+          number: '0123456789',
+        }],
+        [Contacts.Fields.Department]: 'Engineering',
+      };
+      const contactId = await Contacts.addContactAsync(contact); 
+
+      // This line will crash the app with `CNPropertyNotFetchedException` on iOS
+      const { crashData } = await Contacts.getContactByIdAsync(contactId);
+      console.log(crashData);
+
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Contacts Module Example</Text>
     </View>
   );
 }
